b1ea7a243f0ad229ab558ebefdd0d6fa2dedff95
glaucoSapucaia <glaucopco@gmail.com>
2023-12-05 15:02:05 -0300

    Create category/model (foreing key) and register | Update attrs from contact/model

--- contact/admin.py
+++ contact/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from contact import models
 
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('id',)
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'email', 'phone',)
+    list_display = ('id', 'first_name', 'last_name', 'email', 'phone', 'category',)
     search_fields = ('id', 'first_name', 'email',)
     ordering = ('-id',)
     list_display_links = ('id',)
-    list_editable = ('first_name', 'email',)
+    list_editable = ('first_name', 'email', 'category',)
     list_per_page = 10
     list_max_show_all = 100
