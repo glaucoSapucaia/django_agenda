27589fceb1ae2ccaa87421128d402b487d570161
glaucoSapucaia <glaucopco@gmail.com>
2023-12-05 13:44:57 -0300

    Define attrs from contact/model/register

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,10 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'first_name', 'last_name', 'email', 'phone',)
+    search_fields = ('id', 'first_name', 'email',)
+    ordering = ('-id',)
+    list_display_links = ('id',)
+    list_editable = ('first_name', 'email',)
+    list_per_page = 10
+    list_max_show_all = 100
