41b8544f7e8bcafcb442c51b1d1ffbe0b987df0c
glaucoSapucaia <glaucopco@gmail.com>
2023-12-05 20:05:29 -0300

    Update lang html | update context of contact/views/index | Tests with Django query

--- contact/views/views_contact.py
+++ contact/views/views_contact.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from contact import models
 
 def index(request):
-    contacts = models.Contact.objects.all()
+    contacts = models.Contact.objects.filter(show=True).order_by('-id')
     context = {
         'contacts': contacts,
     }
