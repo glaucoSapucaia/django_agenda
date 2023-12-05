4c0450c327b82542a083206fe2b01a79fb23a5d5
glaucoSapucaia <glaucopco@gmail.com>
2023-12-05 19:55:30 -0300

    Create context to contact.index.html | Update base and index html

--- contact/views/views_contact.py
+++ contact/views/views_contact.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from contact import models
 
 def index(request):
+    contacts = models.Contact.objects.all()
+    context = {
+        'contacts': contacts,
+    }
     return render(
         request,
         'contact/index.html',
+        context
     )
