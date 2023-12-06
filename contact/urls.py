e1b7961b6018cfa1b4dac1ce25fbecb585a2adce
glaucoSapucaia <glaucopco@gmail.com>
2023-12-06 19:38:04 -0300

    Create contact.views/urls.forms_contact, create.html template | Use CSRF token

--- contact/urls.py
+++ contact/urls.py
@@ -4,6 +4,10 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
+    # contact.forms urls
+    path('create/', views.create, name='create'),
+
+    # contact.views urls
     path('<int:contact_id>/', views.contact, name='contact'),
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
--- contact/views/__init__.py
+++ contact/views/__init__.py
@@ -1,1 +1,2 @@
 from .views_contact import *
+from .forms_contact import *
