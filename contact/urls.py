05356f04530b4193c19e6b8545ceedf83a6094a4
glaucoSapucaia <glaucopco@gmail.com>
2023-12-07 11:15:32 -0300

    Create contact.delete view and url | Update contact.html with update and delete buttons | Use hidden input logic

--- contact/urls.py
+++ contact/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # contact.forms urls
     path('contact/create/', views.create, name='create'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 
     # contact.views urls
     path('<int:contact_id>/', views.contact, name='contact'),
--- contact/views/forms_contact.py
+++ contact/views/forms_contact.py
@@ -69,3 +69,21 @@ def update(request, contact_id):
         'contact/create.html',
         context,
     )
+
+def delete(request, contact_id):
+    contact = get_object_or_404(models.Contact.objects, pk=contact_id, show=True)
+    confirmation = request.POST.get('confirmation', 'no')
+    context = {
+        'contact': contact,
+        'confirmation': confirmation,
+        'site_title': 'Deletar contato -'
+    }
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+    
+    return render(
+        request,
+        'contact/contact.html',
+        context,
+    )
