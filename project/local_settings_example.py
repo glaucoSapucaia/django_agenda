e05f862ff7febd648cd76788980c30c7f478bc69
glaucoSapucaia <glaucopco@gmail.com>
2023-12-09 14:29:59 -0300

    Config deploy example

---
+++ project/local_settings_example.py
@@ -0,0 +1,13 @@
+SECRET_KEY = 'CHANGE-ME'
+DEBUG = False
+ALLOWED_HOSTS = ['CHANGE-ME']
+DATABASES = {
+    'default': {
+        'ENGINE': 'CHANGE-ME',
+        'NAME': 'CHANGE-ME',
+        'USER': 'CHANGE-ME',
+        'PASSWORD': 'CHANGE-ME',
+        'HOST': 'CHANGE-ME',
+        'PORT': 'CHANGE-ME',
+    },
+},
